b5b631c72eb9143117bc8c674e971af08fbfd928
Miguel Eustaquio <migueladp33@gmail.com>
2025-10-05 20:46:57 -0300

    feat: Refactor SatelliteViewer to include city and state selection for air quality monitoring

--- app/(private)/satellite/page.tsx
+++ app/(private)/satellite/page.tsx
@@ -31,10 +31,9 @@ export default function SatellitePage() {
 
       <main className="max-w-7xl mx-auto p-4 sm:p-6 space-y-4 sm:space-y-6">
         <div className="grid gap-4 sm:gap-6 lg:grid-cols-3">
-          <div className="lg:col-span-2">
+        
             <SatelliteViewer />
-          </div>
-          <SatelliteMap />
+          
         </div>
       </main>
     </div>
